43e79130c8261d8ef63d32e94149bf77c9b99ff3
jacomyou1026 <jacomyou1026@naver.com>
2023-09-18 18:21:10 +0900

    백준 / 완전탐색

--- BackJooN/Brute Force/src/_1436.java
+++ BackJooN/Brute Force/src/_1436.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class _1436 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        int number = 666;
+        int cnt = 1;
+        while (cnt != i) {
+            number++; // 667.. 668.. 669.. ~~ 1666
+            if (String.valueOf(number).contains("666")) {
+                cnt++;
+            }
+        }
+        System.out.println(number);
 
     }
 }
--- BackJooN/Brute Force/src/_3085.java
+++
@@ -1,6 +0,0 @@
-public class _3085 {
-    public static void main(String[] args) {
-        
-
-    }
-}
