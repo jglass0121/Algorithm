d5e9ddafd35e2bdda72382bb2b62b495aef0d1c1
jacomyou1026 <jacomyou1026@naver.com>
2023-12-16 22:35:53 +0900

    숫자_정사각형

--- BackJooN/Implement/src/실버3/숫자정사각형.java
+++ BackJooN/Implement/src/실버3/숫자정사각형.java
@@ -1,44 +1,38 @@
 package 실버3;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class 숫자정사각형 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
 
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-
-        int[][] square = new int[N][M];
-
-        // 입력 받기
+        int[][] arr = new int[N][M];
         for (int i = 0; i < N; i++) {
-            String line = br.readLine();
             for (int j = 0; j < M; j++) {
-                square[i][j] = line.charAt(j) - '0';
+                arr[i][j] = sc.nextInt();
             }
         }
 
-        int answer = 1; // 정답은 최소 1
+        int min = Math.min(N, M);
 
-        // 정사각형의 한 변의 길이를 1부터 증가시키면서 확인
-        for (int size = 1; size < Math.min(N, M); size++) {
-            for (int i = 0; i < N - size; i++) {
-                for (int j = 0; j < M - size; j++) {
-                    // 네 꼭지점의 숫자가 모두 같다면 정사각형이다.
-                    if (square[i][j] == square[i][j + size] &&
-                            square[i][j] == square[i + size][j] &&
-                            square[i][j] == square[i + size][j + size]) {
-                        answer = Math.max(answer, (size + 1) * (size + 1));
+        while (min > 1) {
+            for (int i = 0; i <= N - min; i++) {
+                for (int j = 0; j <= M - min; j++) {
+                    int num = arr[i][j];
+                    if (num == arr[i][j + min - 1] &&
+                            num == arr[i + min - 1][j] &&
+                            num == arr[i + min - 1][j + min - 1]) {
+                        System.out.println(min * min);
+                        return;
                     }
                 }
+
             }
+
+            min--;
         }
 
-        System.out.println(answer);
     }
 }
