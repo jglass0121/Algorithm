daa5ad14626bf56ce91b59a70f6ee99b1f3f6322
jacomyou1026 <jacomyou1026@naver.com>
2024-04-01 23:56:13 +0900

    / 프로그래머스 / L2

--- Programmers/codingTest_kit/src/queue/프로세스.java
+++ Programmers/codingTest_kit/src/queue/프로세스.java
@@ -1,40 +1,5 @@
 package queue;
 
-import java.util.Collections;
-import java.util.PriorityQueue;
-
 public class 프로세스 {
-    public static void main(String[] args) {
-        int[] priorities = {2, 1, 3, 2};
-        int location = 2;
-        solution(priorities, location);
-    }
 
-    private static int solution(int[] priorities, int location) {
-        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
-        int answer = 0;
-
-        // 우선순위 큐에 우선순위 요소 추가
-        for (int i : priorities) {
-            queue.offer(i);
-        }
-
-        // 큐가 빌 때까지 반복
-        while (!queue.isEmpty()) {
-            // 기존 우선순위 배열 순회
-            for (int i = 0; i < priorities.length; i++) {
-                // 현재 작업의 위치 찾기
-                if (queue.peek() == priorities[i]) {
-                    queue.poll();
-                    answer++;
-
-                    // 현재 작업이 location과 같으면 answer 반환
-                    if (location == i) {
-                        return answer;
-                    }
-                }
-            }
-        }
-        return answer;
-    }
 }
