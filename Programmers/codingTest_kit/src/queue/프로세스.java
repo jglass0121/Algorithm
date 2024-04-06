c4162b0e703e75ce4e4ab7ac12a3ed227f987989
jacomyou1026 <jacomyou1026@naver.com>
2024-04-06 19:39:53 +0900

    프로그래머스 / 프로세스

--- Programmers/codingTest_kit/src/queue/프로세스.java
+++ Programmers/codingTest_kit/src/queue/프로세스.java
@@ -1,5 +1,38 @@
 package queue;
 
+import java.util.*;
+
 public class 프로세스 {
+
+    public static void main(String[] args) {
+        int priorities[] = {1, 1, 9, 1, 1, 1};
+        int location = 0;
+
+        solution(priorities, location);
+    }
+
+    private static int solution(int[] priorities, int location) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        int answer = 0;
+
+        for (int i : priorities) {
+            queue.offer(i);
+        }
+
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (queue.peek() == priorities[i]) {
+                    queue.poll();
+                    answer++;
+                    if (location == i) {
+                        return answer;
+                    }
+                }
+            }
+        }
+
+
+        return answer;
+    }
 
 }
