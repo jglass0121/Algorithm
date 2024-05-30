0e4e95545a40f13a479c6ccb9fa6cf2c608726d0
jacomyou1026 <70208747+jacomyou1026@users.noreply.github.com>
2024-05-31 01:22:04 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 3792 ms, Memory: 369172 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static int[] d = new int[300001];
+    public static ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+
+    public static LinkedList<Integer> arr = new LinkedList<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int K = sc.nextInt();
+        int X = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i<= N; i++) {
+            list.add(new ArrayList<Integer>());
+            d[i] = -1;
+        }
+
+        for (int i = 0; i < M; i++) {
+            int from = sc.nextInt();
+            int to = sc.nextInt();
+            list.get(from).add(to);
+            sc.nextLine();
+        }
+
+        bfs( X);
+        boolean check = false;
+        for (int i = 1; i <= N; i++) {
+            if (d[i] == K) {
+                System.out.println(i);
+                check = true;
+            }
+        }
+        if(!check) System.out.println(-1);
+
+    }
+
+    private static void bfs(int start) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(start);
+        d[start] = 0;
+        while (!queue.isEmpty()) {
+            Integer now = queue.poll();
+            for (int i = 0; i < list.get(now).size(); i++) {
+                Integer next = list.get(now).get(i);
+                if (d[next] ==-1) {
+                    d[next] = d[now]+1;
+                    queue.add(next);
+                }
+            }
+
+        }
+    }
+
+
+}
