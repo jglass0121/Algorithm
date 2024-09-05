082c966a8ddcebff08e89472e6f072b305588b2d
jacomyou1026 <jacomyou1026@naver.com>
2024-09-05 23:44:27 +0900

    단어정렬

--- BackJooN/sort/src/실버5/단어정렬.java
+++ BackJooN/sort/src/실버5/단어정렬.java
@@ -1,2 +1,35 @@
-package 실버5;public class 단어정렬 {
+package 실버5;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class 단어정렬 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        String[] arr = new String[num];
+        for (int i = 0; i < num; i++) {
+            arr[i] = sc.nextLine();
+        }
+
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+
+                if (o1.length() == o2.length()) {
+                    return o1.compareTo(o2);
+                }
+                return o1.length()-o2.length();
+            }
+        });
+
+        System.out.println(arr[0]);
+        for (int i = 1; i < num; i++) {
+            if (!arr[i].equals(arr[i - 1])) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
+
 }
