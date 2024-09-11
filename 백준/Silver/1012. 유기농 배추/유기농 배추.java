bef0d7773b8bd5a43a36f09c622061bac447ed89
jacomyou1026 <70208747+jacomyou0121@users.noreply.github.com>
2024-09-11 16:02:02 +0900

    [Silver II] Title: 유기농 배추, Time: 324 ms, Memory: 29252 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,11 +1,11 @@
+
 import java.util.Scanner;
 
 public class Main {
-    static int N;
-    static int M;
     static int[][] arr;
-    static int res = 0;
-
+    static int cnt = 0;
+    static int M;
+    static int N;
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -13,45 +13,40 @@ public class Main {
         sc.nextLine();
         for (int i = 0; i < num; i++) {
             String[] split = sc.nextLine().split(" ");
-            M = Integer.parseInt(split[0]);
-            N = Integer.parseInt(split[1]);
+             M = Integer.parseInt(split[0]);
+             N = Integer.parseInt(split[1]);
             int K = Integer.parseInt(split[2]);
             arr = new int[M][N];
-
             for (int j = 0; j < K; j++) {
-                String[] split1 = sc.nextLine().split(" ");
-                int x = Integer.parseInt(split1[0]);
-                int y = Integer.parseInt(split1[1]);
-                arr[x][y] = 1;
+                String[] xy = sc.nextLine().split(" ");
+                int X = Integer.parseInt(xy[0]);
+                int Y = Integer.parseInt(xy[1]);
+                arr[X][Y] = 1;
             }
 
             for (int j = 0; j < M; j++) {
                 for (int k = 0; k < N; k++) {
                     if (arr[j][k] == 1) {
-                        dfs(j, k);
-                        res += 1;
+                        dfs(j,k);
+                        cnt += 1;
                     }
                 }
             }
-
-
-            System.out.println(res);
-            res = 0;
-
+            System.out.println(cnt);
+            cnt = 0;
         }
+
     }
 
     private static void dfs(int x, int y) {
-        if (x < 0 || x >= M || y < 0 || y >= N) return;
-
-        if (arr[x][y] == 0) return;
+        if(x<0||x>= M||y<0||y>=N) return;
+        if(arr[x][y]==0) return;
         if (arr[x][y] == 1) {
             arr[x][y] = 0;
             dfs(x + 1, y);
-            dfs(x, y + 1);
-            dfs(x - 1, y);
-            dfs(x, y - 1);
-            return;
+            dfs(x , y+1);
+            dfs(x -1, y);
+            dfs(x , y-1);
         }
     }
 }
