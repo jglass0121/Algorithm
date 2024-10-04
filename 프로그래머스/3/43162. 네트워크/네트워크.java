7b4bbdc51851cfb802f0d11311421cffacb8068d
jacomyou1026 <70208747+jacomyou0121@users.noreply.github.com>
2024-10-04 16:40:09 +0900

    [level 3] Title: 네트워크, Time: 0.33 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,45 +1,23 @@
-import java.util.LinkedList;
-
 class Solution {
-    
-        static LinkedList<LinkedList<Integer>> list = new LinkedList<>();
-    static boolean[] visited ;
-    
     public int solution(int n, int[][] computers) {
-          visited = new boolean[n+1];
-        for (int i = 0; i < n+1; i++) {
-            list.add(new LinkedList<Integer>());
-        }
-
-        for (int i = 0; i < computers.length; i++) {
-            for (int j = 0; j < computers[i].length; j++) {
-                if(i==j) continue;
-                if(computers[i][j] == 1){
-                    list.get(i).add(j);
-                }
+        int answer = 0;
+        boolean[] check = new boolean[n];
+        for(int i=0;i<n;i++){
+            if(!check[i]){
+                dfs(computers,i,check);
+                answer++;
             }
         }
-
-        int cnt = 0;
-        for (int i = 0; i <n; i++) {
-            if (!visited[i]) {
-                dfs(i);
-                cnt += 1;
-            }
-        }
-
-        return cnt;
-
+        return answer;
     }
     
-    private static void dfs(int start) {
-
-        visited[start] = true;
-        for (int i = 0; i < list.get(start).size(); i++) {
-            Integer next = list.get(start).get(i);
-            if (!visited[next]) {
-                dfs(next);
+    boolean[] dfs(int[][] computers, int i, boolean[] check){
+        check[i] = true;
+        for(int j =0; j < computers.length;j++){
+            if(i!=j && computers[i][j] == 1 && check[j] == false){
+                check = dfs(computers,j,check);
             }
         }
+        return check;
     }
 }
