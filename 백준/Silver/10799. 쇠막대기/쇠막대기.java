c2aa319f30bd4c1461a89790e6fbd6a7ac3c06c9
jacomyou1026 <70208747+jacomyou0121@users.noreply.github.com>
2024-10-10 18:49:53 +0900

    [Silver II] Title: 쇠막대기, Time: 320 ms, Memory: 22872 KB -BaekjoonHub

---
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        Stack<Character> stack = new Stack<>();
+
+        int res = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') {
+                stack.push('(');   
+                continue;
+            }
+            if (input.charAt(i) == ')') {
+                stack.pop();
+                if (input.charAt(i - 1) == '(') {
+                    res += stack.size();
+                } else {
+                    res++;
+                }
+            }
+        }
+        System.out.println(res);
+    }
+}
