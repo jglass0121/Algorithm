ee5f2b800d035c34e3809cdc21277a294ba4e237
jacomyou1026 <jacomyou1026@naver.com>
2024-10-22 09:59:37 +0900

    문자속단어2

--- java_algorithm_equ/문자열/문자_속_단어.java
+++ java_algorithm_equ/문자열/문자_속_단어.java
@@ -1,24 +1,41 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class 문자_속_단어 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        String[] split = str.split(" ");
+class Main {
 
-
-        int max = 0;
-        String answer  = "";
-        for (int i = 0; i < split.length; i++) {
-            int length = split[i].length();
-            if(max < length){
-                max = length;
-                answer = split[i];
+    public static String solution(String str) {
+        String answer = "";
+        // min이라는 변수와 pos라는 변수를 동시에 선언한 것이고
+        // 그 중 min이라는 변수에는 상수값을 할당해 놓은 것.
+        int min = Integer.MIN_VALUE, pos;
+        // 전달 받은 String에서 공백의 인덱스를 가져온다.
+        // indexOf는 찾는 문자열이 없다면 -1을 리턴하므로 가져온 공백의 인덱스가 -1이 아니면
+        // 문자열에 공백이 있는 것을 의미한다.
+        // 그 다음 처음 부터 공백의 인덱스까지 문자열을 잘라서 해당 문자열의 길이를 할당해 놓는다.
+        // 그 다음 str을 기존의 공백 다음 부터 다시 잘라 다시 반복문으로 들어가서 같은 과정을 반복한다.
+        while ((pos = str.indexOf(" ")) != -1) {
+            String tmp = str.substring(0, pos);
+            int len = tmp.length();
+            if (len > min) {
+                min = len;
+                answer = tmp;
             }
-
+            str = str.substring(pos + 1);
         }
-        System.out.println(answer);
 
+        return answer;
+
+    }
+
+    ;
+
+    public static void main(String[] args) {
+
+
+        // 스캐너 객체를 생성한다.
+        Scanner input = new Scanner(System.in);
+        // 문자열을 입력 받는다.
+        String str = input.nextLine();
+        // solution 메서드에 전달한다.
+        System.out.println(solution(str));
     }
 }
