cc2e278c439f5bbd58db833490f06cf269580308
jacomyou1026 <jacomyou1026@naver.com>
2024-11-15 19:19:24 +0900

    문자열 압축

--- java_algorithm_equ/Array/me/문자열_압축.java
+++ java_algorithm_equ/Array/me/문자열_압축.java
@@ -6,20 +6,27 @@ public class 문자열_압축 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        solution(str);
+        System.out.println(solution(str));
     }
 
     private static String solution(String str) {
         int cnt = 1;
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < str.length()-1; i++) {
-            if (str.charAt(i) == str.charAt(i + 1)) {
+        sb.append(str.charAt(0));
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == str.charAt(i - 1)) {
                 cnt++;
             } else {
+                if (cnt > 1) {
+                    sb.append(cnt);
+                    cnt = 1;
+                }
                 sb.append(str.charAt(i));
-                if(cnt >1) sb.append(cnt);
-                cnt = 1;
+
             }
+        }
+        if (cnt > 1) {
+            sb.append(cnt);
         }
 
         return sb.toString();
