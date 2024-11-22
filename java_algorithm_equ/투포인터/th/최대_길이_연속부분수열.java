b12099cf32d8e6496d525fb4008023ce2f8591a9
jacomyou1026 <jacomyou1026@naver.com>
2024-11-22 15:47:14 +0900

    최대_길이_연속부분수열

---
+++ java_algorithm_equ/투포인터/th/최대_길이_연속부분수열.java
@@ -0,0 +1,36 @@
+package th;
+
+import java.util.Scanner;
+
+public class 최대_길이_연속부분수열 {
+    // 연속된 수열
+    //
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] split = sc.nextLine().split(" ");
+        int N = Integer.parseInt(split[0]);
+        int K = Integer.parseInt(split[1]);
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int solution = solution(N, K, arr);
+        System.out.println(solution);
+
+    }
+
+    private static int solution(int n, int k, int[] arr) {
+        int answer = 0, cnt = 0, lt = 0;
+        for (int rt = 0; rt < n; rt++) {
+            if(arr[rt] == 0) cnt++;
+            while (cnt > k) { // 0의 개수가 k보다 클 경우
+                if(arr[lt] == 0) cnt--; // lt는 1로 변경해서 cnt값 줄이고
+                lt++; // 이동
+            }
+            answer = Math.max(answer, rt - lt + 1);
+        }
+        return answer;
+    }
+}
