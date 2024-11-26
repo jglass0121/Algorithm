2c6ddcbdb5f8c3dc66706a80f1e412db72efefe0
jacomyou1026 <jacomyou1026@naver.com>
2024-11-26 09:18:28 +0900

    최대_길이_연속부분수열

--- java_algorithm_equ/투포인터/th/최대_길이_연속부분수열.java
+++ java_algorithm_equ/투포인터/th/최대_길이_연속부분수열.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 
 public class 최대_길이_연속부분수열 {
     // 연속된 수열
-    //
+    // rt는 무조건 0만나면 1로변경
+    //cnt가 k보다 클경우, lt가 증가하여 0으로 변경되었던 것을 1로 변경 (cnt감소)
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] split = sc.nextLine().split(" ");
@@ -25,7 +27,7 @@ public class 최대_길이_연속부분수열 {
         int answer = 0, cnt = 0, lt = 0;
         for (int rt = 0; rt < n; rt++) {
             if(arr[rt] == 0) cnt++;
-            while (cnt > k) { // 0의 개수가 k보다 클 경우
+            while (cnt > k) {  // 0의 개수가 k보다 클 경우
                 if(arr[lt] == 0) cnt--; // lt는 1로 변경해서 cnt값 줄이고
                 lt++; // 이동
             }
