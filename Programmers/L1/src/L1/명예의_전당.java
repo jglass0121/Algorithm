7e6539723afe5a05bde3ce4ab28472084bd62ea3
jacomyou1026 <jacomyou1026@naver.com>
2025-08-03 17:15:07 +0900

    알고리즘

---
+++ Programmers/L1/src/L1/명예의_전당.java
@@ -0,0 +1,7 @@
+package L1;
+
+public class 명예의_전당 {
+    public static void main(String[] args) {
+
+    }
+}
--- java_algorithm_equ/투포인터/me/공통원소_구하기.java
+++ java_algorithm_equ/투포인터/me/공통원소_구하기.java
@@ -1,44 +1,26 @@
 package me;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class 공통원소_구하기 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
-        int[] arr1 = new int[num];
-        for (int i = 0; i < num; i++) {
-            arr1[i] = sc.nextInt();
-        }
-        int num2 = sc.nextInt();
-        int[] arr2 = new int[num2];
-        for (int i = 0; i < num2; i++) {
-            arr2[i] = sc.nextInt();
+        String[] one = sc.nextLine().split(" ");
+        int[] arr = new int[num];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(one[i]);
         }
 
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-
-        int startOne = 0, startTwo = 0;
-        StringBuilder sb = new StringBuilder();
-
-        while (startOne < num && startTwo < num2) {
-            if (arr1[startOne] == arr2[startTwo]) {
-                sb.append(arr1[startOne]).append(" ");
-                startOne++;
-                startTwo++;
-                continue;
-            }
-            if (arr1[startOne] > arr2[startTwo]) {
-                startTwo++;
-            } else {
-                startOne++;
-            }
+        int tNum = sc.nextInt();
+        String[] two = sc.nextLine().split(" ");
+        int[] arr2 = new int[tNum];
+        for (int i = 0; i < arr2.length; i++) {
+            arr2[i] = Integer.parseInt(two[i]);
         }
-
-        System.out.println(sb.toString());
 
 
     }
+
+
 }
