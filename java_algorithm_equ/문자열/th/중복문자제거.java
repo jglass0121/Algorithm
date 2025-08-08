c2cba63d02cf76533d0e8f37c5b598754d581c68
jacomyou1026 <jacomyou1026@naver.com>
2025-08-08 21:06:17 +0900

    중복문자제거 (th )

--- java_algorithm_equ/문자열/th/중복문자제거.java
+++ java_algorithm_equ/문자열/th/중복문자제거.java
@@ -13,7 +13,7 @@ class 중복_문자_제거 {
     private static String solution(String str) {
         String answer = "";
         for (int i = 0; i < str.length(); i++) {
-            if (str.indexOf(str.charAt(i)) == i) {
+            if (str.indexOf(str.charAt(i)) == i) { //This method returns the first index at which the character
                 answer += str.charAt(i);
             }
         }
