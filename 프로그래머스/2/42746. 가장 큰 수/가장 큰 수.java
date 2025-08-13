430779e1435546ffa2eb738cbfffb641b23d5779
j <70208747+jglass0121@users.noreply.github.com>
2025-08-13 15:58:29 +0900

    [level 2] Title: 가장 큰 수, Time: 264.32 ms, Memory: 130 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,22 +1,26 @@
 import java.util.*;
 class Solution {
     public String solution(int[] numbers) {
-      
-        String[] arr = new String[numbers.length];
-
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = String.valueOf(numbers[i]);
+   // 1. int 배열을 문자열 배열로 변환
+        String[] nums = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            nums[i] = String.valueOf(numbers[i]);
         }
 
+        // 2. 정렬: (o2+o1) vs (o1+o2) 비교
+        Arrays.sort(nums, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
 
-        //내림차순
-        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
-
-        if(arr[0].equals("0")) {return "0";}
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < arr.length; i++) {
-            sb.append(arr[i]);
+        // 3. 정렬 후 첫 값이 "0"이면 모든 값이 0이라는 의미
+        if (nums[0].equals("0")) {
+            return "0";
         }
-        return sb.toString();
+
+        // 4. 문자열 합치기
+        StringBuilder answer = new StringBuilder();
+        for (String num : nums) {
+            answer.append(num);
+        }
+
+        return answer.toString();
     }
 }
