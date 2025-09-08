4719af177ea4aa92e9d8aa15613a7abac1a91c14
Jeon <70208747+jglass0121@users.noreply.github.com>
2025-09-08 21:03:36 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 36.91 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,18 +1,17 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int []arr) {
-        LinkedList list = new LinkedList<Integer>();
+    public int[] solution(int []arr) {  int prev = arr[0];
+        LinkedList<Integer> list = new LinkedList<>();
         list.add(arr[0]);
-        int first = arr[0];
-        for(int i = 1; i < arr.length; i++){
-            int second = arr[i];
-            if(first == second) continue;
-            list.add(arr[i]);
-            first = arr[i];
+        for (int i = 1; i < arr.length; i++) {
+            if (prev != arr[i]) {
+                prev = arr[i];
+                list.add(arr[i]);
+            }
         }
-return list.stream()
-           .mapToInt(i -> ((Integer) i).intValue())  // Object를 Integer로 캐스팅
-           .toArray();
+
+        int[] array = list.stream().mapToInt(Integer::intValue).toArray();
+        return array;
     }
 }
