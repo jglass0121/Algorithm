b61dec7ec9a8e902323c852d1cb875143d0a19ca
jacomyou1026 <jacomyou1026@naver.com>
2025-10-24 19:28:32 +0900

    문자열 뒤집기

---
+++ java_algorithm_equ/etc/문자열_뒤집기.java
@@ -0,0 +1,10 @@
+public class 문자열_뒤집기 {
+    public static void main(String[] args) {
+        String str = "hello";
+        String reverse = "";
+        for (int i = str.length()-1; i >= 0; i--) {
+            reverse += ""+str.charAt(i);
+        }
+        System.out.println("reverse = " + reverse);
+    }
+}
