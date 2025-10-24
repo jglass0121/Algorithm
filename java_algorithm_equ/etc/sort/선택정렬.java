9e40b1e76fb2f827ba3284e67f5f26963dea1bd2
jacomyou1026 <jacomyou1026@naver.com>
2025-10-24 20:01:38 +0900

    선택정렬

---
+++ java_algorithm_equ/etc/sort/선택정렬.java
@@ -0,0 +1,21 @@
+package sort;
+
+public class 선택정렬 {
+    public static void main(String[] args) {
+        int[] arr = {7, 5, 9, 0, 3, 1, 6, 2, 4, 8};
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+        }
+
+        for (int n : arr) {
+            System.out.print(n+" ");
+        }
+    }
+}
