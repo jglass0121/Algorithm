d8af7d1172f8b1af0a9b099a0634e81762484dbe
jacomyou1026 <jacomyou1026@naver.com>
2025-11-06 21:45:17 +0900

    stack

--- java_algorithm_equ/해쉬/th/K번쨰_큰수.java
+++ java_algorithm_equ/해쉬/th/K번쨰_큰수.java
@@ -12,7 +12,7 @@ public class K번쨰_큰수 {
         int n = sc.nextInt();
         int k = sc.nextInt();
         int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i  < n; i++) {
             arr[i] = sc.nextInt();
         }
         System.out.println(solution(arr,n,k));
