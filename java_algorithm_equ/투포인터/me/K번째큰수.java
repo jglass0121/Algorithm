57ac551f84053d2c69e5977d301ce851273a5b82
user <jacomyou1026@naver.com>
2025-11-09 18:11:12 +0900

    d

---
+++ java_algorithm_equ/투포인터/me/K번째큰수.java
@@ -0,0 +1,17 @@
+package me;
+
+import java.util.Scanner;
+
+public class K번째큰수 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int[] arr = new int[K];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+    }
+}
