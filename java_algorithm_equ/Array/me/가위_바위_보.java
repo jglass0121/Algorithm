0ffbc592a531d01073da8443579bcde3266914e5
jglass0121 <jacomyou1026@naver.com>
2026-01-06 10:58:42 +0900

    가위바위보

--- java_algorithm_equ/Array/me/가위_바위_보.java
+++ java_algorithm_equ/Array/me/가위_바위_보.java
@@ -7,6 +7,7 @@ public class 가위_바위_보 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int num = sc.nextInt();
+        sc.nextLine();
         int[] arr1 = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
         int[] arr2 = Arrays.stream(sc.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
 
@@ -24,8 +25,35 @@ public class 가위_바위_보 {
             // 1 - 가위 2 바위 3 보
             //1 3 - 1
             // 2  1
+            if(a == 1){
+                if(b == 1){
+                    sb.append("D").append("\n");
+                } else if (b == 2) {
+                    sb.append("B").append("\n");
+                } else {
+                    sb.append("A").append("\n");
+                }
+            }else if(a == 2){
+                if(b == 1){
+                    sb.append("A").append("\n");
+                } else if (b == 2) {
+                    sb.append("D").append("\n");
+                } else {
+                    sb.append("B").append("\n");
+                }
+            } else if (a == 3) {
+                if(b == 1){
+                    sb.append("B").append("\n");
+                } else if (b == 2) {
+                    sb.append("A").append("\n");
+                } else {
+                    sb.append("D").append("\n");
+                }
+            }
+
 
         }
+        System.out.println(sb.toString());
 
 
     }
