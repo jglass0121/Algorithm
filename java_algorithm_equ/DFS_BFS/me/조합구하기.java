e60ea30972b1480f13571469e308cc8f1b10fd66
jobforjin <jacomyou1026@naver.com>
2026-02-10 19:07:44 +0900

    조합구하기

--- java_algorithm_equ/DFS_BFS/me/조합구하기.java
+++ java_algorithm_equ/DFS_BFS/me/조합구하기.java
@@ -17,21 +17,19 @@ public class 조합구하기 {
     static int M;
 
     private static void fac(int depth, int s) {
+
         if (depth == M) {
-            for (int x : ch) {
-                System.out.print(x+" ");
-                System.out.println();
+            for (int i = 0; i < ch.length; i++) {
+                System.out.print (ch[i]);
             }
             return;
         }
 
         for (int i = s; i <= N; i++) {
-            // ㅈ
-            ch[i] = i;
-            fac(depth + 1, s + 1);
-
+            ch[depth] =i;
+            fac(depth+1,i+1);
         }
-
+        
     }
 
 }
