0ede5cbaa0c8ebb101c883d2c7b85e122e83461f
jobforjin <jacomyou1026@naver.com>
2026-02-25 10:41:24 +0900

    합이_같은_부분_집합2

---
+++ java_algorithm_equ/DFS_BFS/me/two/합이_같은_부분_집합.java
@@ -0,0 +1,37 @@
+package me.two;
+
+import java.util.Scanner;
+
+public class 합이_같은_부분_집합 {
+    static String answer = "NO";
+    static int n, total = 0;
+    static boolean flag = false;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+            total += arr[i];
+        }
+        DFS(0, 0, arr);
+        System.out.println(answer);
+
+    }
+
+    private static void DFS(int L, int sum, int[] arr) {
+        if(flag) return;
+        if(sum > total /2) return;
+        if(L == n) {
+            if((total - sum) == sum){ // 반값이라면
+                answer = "YES";
+                flag = true;
+            }
+
+        }else{
+            DFS(L + 1, sum + arr[L], arr);
+            DFS(L + 1, sum , arr);
+        }
+
+    }
+}
