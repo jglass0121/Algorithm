f60fe5af23857399ad103435b613bf951902aac7
J <70208747+jglass0121@users.noreply.github.com>
2026-03-26 15:32:46 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 32.21 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,35 +1,43 @@
 import java.util.*;
 class Solution {
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
-         Queue<Integer> bridge = new LinkedList<>();
-        int time = 0;
-        int currentWeight = 0;
-        int index = 0;
+    public int solution(int bridgeLength, int weight, int[] truckWeights) {
+       
+        Queue<Integer> queue = new LinkedList<>();
+        Queue<Integer> bridge = new LinkedList<>();
 
-        // 초기에는 다리 길이만큼 0으로 채워둠
-        for (int i = 0; i < bridge_length; i++) {
+        for (int i = 0; i < truckWeights.length; i++) {
+            queue.add(truckWeights[i]);
+        }
+
+        // 다리 초기화
+        for (int i = 0; i < bridgeLength; i++) {
             bridge.add(0);
         }
 
-        while (index < truck_weights.length) {
-            time++;
-            
-            // 한 칸 이동: 맨 앞 트럭 빠져나감
-            currentWeight -= bridge.poll();
+        int cnt = 0;
+        int totalWeight = 0;
 
-            // 다음 트럭 다리에 올릴 수 있으면
-            if (currentWeight + truck_weights[index] <= weight) {
-                bridge.add(truck_weights[index]);
-                currentWeight += truck_weights[index];
-                index++;
+        while (!queue.isEmpty()) {
+            cnt++;
+
+            // 1️⃣ 다리에서 하나 빠짐
+            totalWeight -= bridge.poll();
+
+            Integer nextCar = queue.peek();
+
+            // 2️⃣ 다음 트럭 올릴 수 있는지 확인
+            if (totalWeight + nextCar <= weight) {
+                Integer next = queue.poll();
+                bridge.add(next);
+                totalWeight += next;
             } else {
-                // 못 올리면 빈 칸 추가
                 bridge.add(0);
             }
         }
 
-        // 마지막 트럭이 다리 끝까지 지나가는 시간 더하기
-        time += bridge_length;
-        return time;
+        // 남은 다리 길이만큼 시간 추가
+        cnt += bridgeLength;
+
+        return cnt;
     }
 }
